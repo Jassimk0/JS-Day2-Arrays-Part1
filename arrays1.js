aa429c6949a6373a1662d1ddba0470b4112a81e3
Jassim ALkhadhari <Jassimk08@gmail.comm>
2024-08-06 07:52:50 -0700

    Arrays1

---
+++ arrays1.js
@@ -0,0 +1,10 @@
+grocries = ["milk", "water", "eggs", "turkey", "bread", "cheese"];
+grocries.push("chicken", "steak");
+grocries.pop(1);
+grocries.slice(0,3);
+
+console.log(grocries);
+
+firstThreeItems = grocries.slice(0,3);
+
+console.log(firstThreeItems);
